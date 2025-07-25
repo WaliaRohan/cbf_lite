a4f5679b4bc5cc7ce3c4ef23602856eb1a97f636
WaliaRohan <walia.rohan@gmail.com>
2025-07-25 19:17:15 -0400

    EKF/GEKF: Kalman gain is now multiplied properly (this is probably why partial obs was not working)

--- estimators.py
+++ estimators.py
@@ -15,7 +15,7 @@ class EKF:
     def __init__(self, dynamics, dt, h = None, x_init=None, P_init=None, Q=None, R=None):
         self.dynamics = dynamics  # System dynamics model
         self.dt = dt  # Time step
-        self.K = jnp.zeros((dynamics.state_dim, dynamics.state_dim))
+        self.K = jnp.zeros((dynamics.state_dim, dynamics.state_dim)) # Ideally, it's shape should be (dynamics.state_dim, obs_dim)
         self.name = "EKF"
 
         # Initialize belief (state estimate)
@@ -62,10 +62,14 @@ class EKF:
         # H_x = jnp.array([[0.0, 1.0, 0.0, 0.0]])# can only observe y
 
         H_x = jax.jacfwd(self.h)(self.x_hat)
-        y = z - self.h(self.x_hat) # Innovation term: note self.x_hat comes from identity observation model
+        obs_dim = len(H_x) # Number of rows in H_X == observation space dim
+
+        z_obs = self.h(z) # This might not be technically correct, but here I am just extracting the second state from the measurement
+
+        y = (z_obs - self.h(self.x_hat)) # Innovation term: note self.x_hat comes from identity observation model
+        y = jnp.reshape(y, (obs_dim, 1)) 
 
         # Innovation Covariance
-        obs_dim = len(H_x) # Number of rows in H_X == observation space dim
         S = H_x @ self.P @ H_x.T + self.R[:obs_dim, :obs_dim]# self.
 
         # Handle degenerate S cases
@@ -80,7 +84,7 @@ class EKF:
         self.in_cov = self.K @ S @ self.K.T
 
         # Update state estimate
-        self.x_hat = self.x_hat + self.K@y
+        self.x_hat = self.x_hat + (self.K@y).reshape(self.x_hat.shape) # Order of K and y in multiplication matters!
 
         self.sigma_minus = self.P # for computing probability bound
 
@@ -133,7 +137,7 @@ class GEKF:
         # Covariance initialization
         self.P = P_init if P_init is not None else jnp.eye(dynamics.state_dim) * 0.1  
         self.Q = dynamics.Q # Process noise covariance
-        self.R = sigma_v*jnp.eye(dynamics.state_dim) if R is not None else jnp.eye(dynamics.state_dim) * 0.05  # Measurement noise covariance
+        self.R = R if R is not None else jnp.square(sigma_v)*jnp.eye(dynamics.state_dim) * 0.05  # Measurement noise covariance
 
         self.in_cov = jnp.zeros((dynamics.state_dim, dynamics.state_dim)) # For tracking innovation covariance
         self.sigma_minus = self.P
@@ -154,37 +158,32 @@ class GEKF:
         self.P = self.P + P_dot*self.dt
 
     def update(self, z):
-        """Measurement update step of EKF."""
-        """z: measurement"""
-        mult_state = 0
-
+        """
+        Measurement update step of GEKF.
+        z: measurement
+        """
         mu_u = self.mu_u
         sigma_u = self.sigma_u
 
         mu_v = self.mu_v
-        sigma_v = self.sigma_v
       
         # Perfect state observation
         h_z = self.x_hat
-        dhdx = jnp.eye(self.dynamics.state_dim) # change name of this variable
+        dhdx = jnp.eye(self.dynamics.state_dim)
 
-        h_z = h_z.at[mult_state].set(h_z[mult_state] * (1 + mu_u))
-        E = h_z + mu_v
+        E = (1 + mu_u)*h_z + mu_v # This is the "observation function output" for GEKF
 
-        C = jnp.matmul(self.P, jnp.transpose(dhdx, axes=None))  # Perform the matrix multiplication
-        C = C.at[mult_state].set(C[mult_state]*(1 + mu_u))  # Modify only the specified state
+        C = (1 + mu_u)*jnp.matmul(self.P, jnp.transpose(dhdx))  # Perform the matrix multiplication
         
         M = jnp.diag(jnp.diag(jnp.matmul(dhdx, jnp.matmul(self.P, jnp.transpose(dhdx))) + jnp.matmul(h_z, jnp.transpose(h_z))))
         
-        S = jnp.matmul(dhdx, jnp.matmul(self.P, jnp.transpose(dhdx, axes=None)))  # Perform matrix multiplication
-        S = S.at[mult_state].set(S[mult_state]*jnp.square(1 + mu_u))  # Apply (1 + mu_u)^2 to the specified state
-        M = M.at[1, 1].set(M[1, 1]*jnp.square(sigma_u))
-        S = S + M + jnp.square(sigma_v)
+        S_term_1 = jnp.square(1 + mu_u)*jnp.matmul(dhdx, jnp.matmul(self.P, jnp.transpose(dhdx)))  # Perform matrix multiplication
+        S = S_term_1 + jnp.square(sigma_u)*M + self.R
 
         self.K = jnp.matmul(C, jnp.linalg.inv(S))
 
         # Update state estimate
-        self.x_hat = self.x_hat + jnp.matmul(z - E, self.K)
+        self.x_hat = self.x_hat + jnp.transpose(jnp.matmul(self.K, jnp.transpose(z - E))) # double transpose because of how state is defined.
 
         # Update covariance
         self.P = self.P - jnp.matmul(self.K, jnp.transpose(C, axes=None))
