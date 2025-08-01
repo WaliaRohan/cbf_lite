pre merge to main: Fixed dimension issue in obs func jacobian

cbfs.py
@@ -132,9 +132,9 @@ def clf_1D_doubleint(state, goal):
     #     MU: Helps in convergence to goal when system is near it. Near the goal, the "diff" (see below) might be close to zero. So this
               # relies on velocity to help the system reach the goal. 
 
-    GAMMA = 475 # 5 
-    LAMBDA = 0.25 # 1
-    MU = 30.0 # 1 
+    GAMMA = 2.4
+    LAMBDA = 1.0
+    MU = 1.0 
     lyap = (GAMMA*diff + LAMBDA*x_dot)**2 + MU*x_dot**2
 
     return lyap[0]
@@ -163,7 +163,7 @@ class BeliefCBF:
         delta: probability of failure (we want the system to have a probability of failure less than delta)
         n: dimension of the state space
         """
-        self.alpha = alpha.reshape(-1, 1)
+        self.alpha = alpha.reshape(-1, 1) # reshape into column vector
         self.beta = beta
         self.delta = delta
         self.n = n
@@ -187,7 +187,7 @@ class BeliefCBF:
         upper_triangular_indices = jnp.triu_indices(sigma.shape[0])
         vec_sigma = sigma[upper_triangular_indices]
 
-        b = jnp.concatenate([mu.squeeze(), vec_sigma])
+        b = jnp.concatenate([mu.flatten(), vec_sigma]) # mu.squeeze() would not work for shapes of size (1, 1) (it deletes all 1 dimensions). mu.flatten() makes final shape (n, ), regardless of original shape. 
 
         return b
     
@@ -213,29 +213,31 @@ class BeliefCBF:
             
             sigma_matrix: f_sigma or g_sigma
             """
-            shape = sigma_matrix.shape  # G_sigma is (n, m, n)
-            n = shape[0]
-            m = shape[1]
 
-            # Create indices for the upper triangular part
-            tri_indices = jnp.triu_indices(n)
+            if sigma_matrix.shape == (1, 1):
+                return sigma_matrix.flatten()
 
-            # Extract upper triangular elements from each m-th slice
-            sigma_vector = jnp.array([sigma_matrix[:, j][tri_indices] for j in range(m)]).T  # Shape (k, m)
-            
-            return sigma_vector
+            else:
+                shape = sigma_matrix.shape  # G_sigma is (n, m, n)
+                n = shape[0]
+                m = shape[1]
 
+                # Create indices for the upper triangular part
+                tri_indices = jnp.triu_indices(n)
+
+                # Extract upper triangular elements from each m-th slice
+                sigma_vector = jnp.array([sigma_matrix[:, j][tri_indices] for j in range(m)]).T # TODO: Check if this is valid for m > 1
+                
+                return sigma_vector
 
         def f_b(b):
             # Time update evaluated at mean
             f_vector = dynamics.f(b[:self.n]) 
 
-            # Covariance update evaluated at mean
-            A_f = jax.jacfwd(dynamics.f)(b[:self.n])
-            # f_sigma = A_f@sigma + sigma@(A_f.T) + dynamics.Q
-            f_sigma = A_f @ sigma + A_f.T @ sigma # Add noise (dynamics.Q) later
-            # upper_triangular_indices = jnp.triu_indices(f_sigma.shape[0])
-            # f_sigma_vector = f_sigma[upper_triangular_indi?ces]
+            # Calculate A_f: This splitting of A into A_f is possible because sigma dot is control affine - see para after eq 43 in "Belief Space Planning ..." by Nishimura and Schwager
+            A_f = jax.jacfwd(dynamics.f)(b[:self.n]) # jacfwd returns jacobian
+            # Continuous time update - Optimal and Robust Control, Page 154, Eq 3.21
+            f_sigma = A_f @ sigma + A_f.T @ sigma # Add noise (dynamics.Q) later -> Does it even matter if we're ultimately calculating dh/db (eq 19, BCBF paper)
             f_sigma_vector = extract_sigma_vector(f_sigma)
             
             # "f" portion of belief vector
@@ -251,22 +253,22 @@ class BeliefCBF:
                 n = shape[0]
                 m = shape[1]
 
-
                 # Create indices for the upper triangular part
                 tri_indices = jnp.triu_indices(n)
 
                 # Extract upper triangular elements from each m-th slice
-                g_sigma = jnp.array([G_sigma[:, j][tri_indices] for j in range(m)]).T  # Shape (k, m)
+                g_sigma = jnp.array([G_sigma[:, j][tri_indices] for j in range(m)]).T # TODO: Check if this is valid for m > 1
                 
                 return g_sigma
 
             # Control influence on mean
             g_matrix = dynamics.g(b[:self.n])
             
-            # Covariance update term
-            A_g = jax.jacfwd(dynamics.g)(b[:self.n]) # Squeeze added later
+            # Calculate A_g: This splitting of A into A_g is possible because sigma dot is control affine - see para after eq 43 in "Belief Space Planning ..." by Nishimura and Schwager
+            A_g = jax.jacfwd(dynamics.g)(b[:self.n]) # jacfwd returns jacobian
             # A_g = A_g.transpose(0, 2, 1) # nxnxm -> Remove if this is giving incorrect results (belief cbf was working for 2d dynamics before this was added for 4x1 dubins)
-            g_sigma = A_g @ sigma + (A_g.T)@sigma  # No Q -> accounted for in f
+            # Continuous time update - Optimal and Robust Control, Page 154, Eq 3.21
+            g_sigma = A_g @ sigma + (A_g.T)@sigma  # No Q -> see "f_b" above
             g_sigma_vector = extract_g_sigma(g_sigma)
 
             # "g" portion of belief vector
@@ -298,7 +300,7 @@ class BeliefCBF:
 
                 where:
                     h_dot = LfH
-                    h: position-based Belief Barrier Function constraint
+                    h: position-based Belief Control Barrier Function constraint
 
         This function calculates the right-hand-side (RHS) of the following
         resulting QP linear inequality:
@@ -313,8 +315,6 @@ class BeliefCBF:
         
         """        
         roots = jnp.array([-0.75]) # Manually select root to be in left half plane
-        # polynomial = np.poly1d(roots, r=True)
-        # coeff = jnp.array(polynomial.coeffs)
         coeff = cbf_gain*jnp.poly(roots)
 
         # jax.debug.print("Value: {}", coeff)

estimators.py
@@ -26,24 +26,26 @@ class EKF:
 
         # Initialize observation function as identity function
         if h is None:
-            self.h = lambda x: x
+            self.h = lambda x: x.ravel()
         else:
             self.h = h
 
     def predict(self, u):
-        """Predict step of EKF."""
+        """
+        Predict step of EKF.
+        
+        See (Page 274, Table 5.1, Optimal and Robust Estimation)
+        """
         # Nonlinear state propagation
-        # self.x_hat = self.x_hat + self.dt * (self.dynamics.f(self.x_hat) + self.dynamics.g(self.x_hat) @ u)
         self.x_hat = self.x_hat + self.dt * self.dynamics.x_dot(self.x_hat, u)
 
         # Compute Jacobian of dynamics (linearization)
-        # F_x = jax.jacobian(lambda x: x + self.dt * (self.dynamics.f(x) + self.dynamics.g(x) @ u))(self.x_hat)
         F = jax.jacobian(lambda x: (self.dynamics.x_dot(x, u).squeeze()))(self.x_hat)
 
-        # Covariance udpate  (Page 274, Table 5.1, Optimal and Robust Estimation)
-        # self.P = F_x @ self.P @ F_x.T + self.Q
-        # P_dot = (F @ self.P + F.T @ self.P).squeeze() + self.Q
-        P_dot = F.squeeze() @ self.P + self.P @ F.squeeze().T + self.Q
+        # Covariance udpate 
+        if len(F.shape) > 2: 
+            F = F.squeeze()
+        P_dot = F @ self.P + self.P @ F.T + self.Q
         self.P = self.P + P_dot*self.dt
 
     def update(self, z):
@@ -53,10 +55,13 @@ class EKF:
         Args:
             z (): Measurement
         """
-        # H_x Jacobian of measurement function wrt state vector (identity for full state observation)
-        # H_x = jnp.array([[0.0, 1.0, 0.0, 0.0]])# can only observe y
-
-        H_x = jax.jacfwd(self.h)(self.x_hat)
+        # H_x Jacobian of measurement function wrt state vector
+        H_x = jax.jacfwd(self.h)(self.x_hat.ravel()) # The output of this should be (obs_dim, state_vector_len). 
+        """
+        NOTE: If H_x's shape is not (obs_dim, state_vector_len), ensure that the "h" operates on 1-dimensional
+        state vector (x_dim, ) and the input (state vector value) at which jacobian needs to be calculted is also dimensionless.
+        """
+        
         obs_dim = len(H_x) # Number of rows in H_X == observation space dim
 
         z_obs = self.h(z) # This might not be technically correct, but here I am just extracting the second state from the measurement
@@ -81,10 +86,10 @@ class EKF:
         # Update state estimate
         self.x_hat = self.x_hat + (self.K@y).reshape(self.x_hat.shape) # Order of K and y in multiplication matters!
 
-        self.sigma_minus = self.P # for computing probability bound
+        self.sigma_minus = self.P # For computing probability bound
 
         # Update covariance
-        self.P = (jnp.eye(max(z.shape)) - self.K @ H_x) @ self.P
+        self.P = (jnp.eye(max(self.x_hat.shape)) - self.K @ H_x) @ self.P
 
     def get_belief(self):
         """Return the current belief (state estimate)."""
@@ -144,17 +149,21 @@ class GEKF:
             self.h = h
 
     def predict(self, u):
-        """Predict step of EKF."""
+        """
+        Same as predict step of EKF.
+        
+        See (Page 274, Table 5.1, Optimal and Robust Estimation)        
+        """
         # Nonlinear state propagation
         self.x_hat = self.x_hat + self.dt * self.dynamics.x_dot(self.x_hat, u)
 
         # Compute Jacobian of dynamics (linearization)
         F = jax.jacobian(lambda x: (self.dynamics.x_dot(x, u).squeeze()))(self.x_hat)
 
-        # Covariance udpate  (Page 274, Table 5.1, Optimal and Robust Estimation)
-        # P_dot = F @ self.P + self.P@ (F.T) + self.Q
-        # P_dot = (F @ self.P + F.T @ self.P).squeeze() + self.Q
-        P_dot = F.squeeze() @ self.P + self.P @ F.squeeze().T + self.Q
+        # Covariance udpate
+        if len(F.shape) > 2: 
+            F = F.squeeze()
+        P_dot = F @ self.P + self.P @ F.T + self.Q
 
         self.P = self.P + P_dot*self.dt
 
@@ -167,12 +176,14 @@ class GEKF:
         sigma_u = self.sigma_u
         mu_v = self.mu_v
 
-        H_x = jax.jacfwd(self.h)(self.x_hat)
-        obs_dim = len(H_x) # Number of rows in H_X == observation space dim
+        # H_x Jacobian of measurement function wrt state vector
+        H_x = jax.jacfwd(self.h)(self.x_hat.ravel()) # The output of this should be (obs_dim, state_vector_len). 
+        """
+        NOTE: If H_x's shape is not (obs_dim, state_vector_len), ensure that the "h" operates on 1-dimensional
+        state vector (x_dim, ) and the input (state vector value) at which jacobian needs to be calculted is also dimensionless.
+        """
 
-        # Perfect state observation
-        # h_z = self.x_hat
-        # dhdx = jnp.eye(self.dynamics.state_dim)
+        obs_dim = len(H_x) # Number of rows in H_X == observation space dim
 
         z_obs = self.h(z) # This might not be technically correct, but here I am just extracting the second state from the measurement
 

sim_belief_cbf_2D_dubins.py
@@ -16,7 +16,7 @@ from sensor import noisy_sensor_mult as sensor
 
 # Sim Params
 dt = 0.001
-T = 5000 # 5000
+T = 10000 # 5000
 dynamics = DubinsDynamics()
 
 # Sensor Params
@@ -41,8 +41,8 @@ x_initial_measurement = sensor(x_true, 0, mu_u, sigma_u, mu_v, sigma_v) # mult_n
 # Observation function: Return second and 4rth element of the state vector
 # self.h = lambda x: x[jnp.array([1, 3])]
 h = lambda x: jnp.array([x[1]])
-estimator = GEKF(dynamics, dt, mu_u, sigma_u, mu_v, sigma_v, h=h, x_init=x_initial_measurement)
-# estimator = EKF(dynamics, dt, h=h, x_init=x_initial_measurement, R=jnp.square(sigma_v)*jnp.eye(dynamics.state_dim))
+# estimator = GEKF(dynamics, dt, mu_u, sigma_u, mu_v, sigma_v, h=h, x_init=x_initial_measurement)
+estimator = EKF(dynamics, dt, h=h, x_init=x_initial_measurement, R=jnp.square(sigma_v)*jnp.eye(dynamics.state_dim))
 
 # Define belief CBF parameters
 n = dynamics.state_dim
@@ -56,7 +56,7 @@ u_max = 5.0
 clf_gain = 20.0 # CLF linear gain
 clf_slack_penalty = 100.0
 cbf_gain = 10.0  # CBF linear gain
-CBF_ON = False
+CBF_ON = True
 
 # Autodiff: Compute Gradients for CLF
 grad_V = grad(clf, argnums=0)  # ∇V(x)
@@ -148,6 +148,7 @@ prob_leave = [] # Probability of leaving safe set
 x_estimated, p_estimated = estimator.get_belief()
 
 x_measured = x_initial_measurement
+x_meas.append(x_measured)
 
 solve_qp_cpu = jit(solve_qp, backend='cpu')
 
@@ -198,6 +199,8 @@ for t in tqdm(range(T), desc="Simulation Progress"):
         if estimator.name == "EKF":
             estimator.update(x_measured)
 
+        x_meas.append(x_measured)
+
         # prob_leave.append(estimator.compute_probability_bound(alpha, delta))
     # else:
     #     if len(prob_leave) > 0:
@@ -209,7 +212,6 @@ for t in tqdm(range(T), desc="Simulation Progress"):
 
     # Store for plotting
     u_traj.append(u_opt)
-    x_meas.append(x_measured)
     x_est.append(x_estimated)
     kalman_gains.append(estimator.K)
     covariances.append(p_estimated)
@@ -220,14 +222,15 @@ x_traj = jnp.array(x_traj)
 
 # Convert to numpy arrays for plotting
 x_traj = np.array(x_traj).squeeze()
-# x_meas = np.array(x_meas).squeeze()
+x_meas = np.array(x_meas).squeeze()
 x_est = np.array(x_est).squeeze()
 
-time = dt*np.arange(T)  # assuming x_meas.shape[0] == N
+time = dt*np.arange(T)
 
 # Plot trajectory with y-values set to zero
 plt.figure(figsize=(10, 10))
-# plt.plot(x_meas[:, 0], x_meas[:, 1], color="Green", linestyle=":", label="Measured Trajectory")
+# plt.plot(x_meas[:, 0], x_meas[:, 1], color="Green", linestyle=":", label="Measured Trajectory", alpha=0.5)
+plt.scatter(x_meas[:, 0], x_meas[:, 1], color="Green", marker="o", s=5, label="Measured", alpha=0.5)
 plt.plot(x_traj[:, 0], x_traj[:, 1], "b-", label="Trajectory (True state)")
 plt.plot(x_est[:, 0], x_est[:, 1], "Orange", label="Estimated Trajectory")
 plt.axhline(y=wall_y, color="red", linestyle="dashed", linewidth=1, label="Obstacle")
@@ -235,9 +238,31 @@ plt.axhline(y=goal[1], color="purple", linestyle="dashed", linewidth=1, label="G
 # plt.scatter(goal[0], goal[1], c="g", marker="*", s=200, label="Goal")
 plt.xlabel("x", fontsize=14)
 plt.ylabel("y", fontsize=14)
-plt.title("2D X-Trajectory (CLF-CBF QP-Controlled)", fontsize=14)
+plt.title("2D System Trajectory", fontsize=14)
 plt.legend()
 plt.grid()
+plt.show()
+
+# Dimension-wise trajectory plotting
+state_labels = ["x", "y", "v", "theta"]
+T = x_traj.shape[0]
+meas_indices = np.arange(0, T, 1/sensor_update_frequency)
+
+plt.figure(figsize=(12, 10))
+for i in range(4):
+    plt.subplot(2, 2, i + 1)
+    plt.plot(range(T), x_traj[:, i], label="True", color="blue")
+    plt.plot(range(T), x_est[:, i], label="Estimated", color="orange")
+    # plt.plot(range(T), x_meas[:, i], label="Measured", color="green", linestyle=":", alpha=0.5)
+    plt.scatter(meas_indices, x_meas[:, i], color="Green", marker="o", s=5, label="Measured", alpha=0.5)
+    plt.xlabel("Time step", fontsize=12)
+    plt.ylabel(state_labels[i], fontsize=12)
+    plt.title(f"{state_labels[i]} vs Time", fontsize=12)
+    plt.grid()
+    if i == 0:
+        plt.legend()
+
+plt.tight_layout()
 plt.show()
 
 # # Plot controls

sim_belief_cbf_2D_dubins_sinusoidal.py
@@ -44,8 +44,8 @@ x_initial_measurement = sensor(x_true, 0, mu_u, sigma_u, mu_v, sigma_v) # mult_n
 # Observation function: Return second and 4rth element of the state vector
 # self.h = lambda x: x[jnp.array([1, 3])]
 h = lambda x: jnp.array([x[1]])
-estimator = GEKF(dynamics, dt, mu_u, sigma_u, mu_v, sigma_v, h=h, x_init=x_initial_measurement)
-# estimator = EKF(dynamics, dt, h=h, x_init=x_initial_measurement, R=jnp.square(sigma_v)*jnp.eye(dynamics.state_dim))
+# estimator = GEKF(dynamics, dt, mu_u, sigma_u, mu_v, sigma_v, h=h, x_init=x_initial_measurement)
+estimator = EKF(dynamics, dt, h=h, x_init=x_initial_measurement, R=jnp.square(sigma_v)*jnp.eye(dynamics.state_dim))
 
 # Define belief CBF parameters
 n = dynamics.state_dim

sim_belief_cbf_double_integrator_1D.py
@@ -15,16 +15,17 @@ from sensor import noisy_sensor_mult as sensor
 
 # from sensor import ubiased_noisy_sensor as sensor
 
-# Define simulation parameters
-# dt = 0.001 # Time step
-# T = 20 # Number of steps
-# dt = 1
-# T = 5
-
+# Sim Params
 dt = 0.001
-T = 4000 # 5000
+T = 5000
+dynamics = LinearDoubleIntegrator1D()
 
-u_max = 6.0
+# Sensor Params
+mu_u = 0.1
+sigma_u = jnp.sqrt(0.01)
+mu_v = 0.001
+sigma_v = jnp.sqrt(0.0005)
+sensor_update_frequency = 10 # how many sim iterations to skip for sensor measurement
 
 # Obstacle
 wall_x = 6.0
@@ -36,21 +37,10 @@ x_true = jnp.array([x_init])  # Start position
 goal = jnp.array([goal_x])  # Goal position
 obstacle = jnp.array([wall_x])  # Wall
 
-sensor_update_frequency = 0.1 # Hz
-
-dynamics = LinearDoubleIntegrator1D()
-
-# Mean and covariance
-mu_u = 0.1
-sigma_u = jnp.sqrt(0.01)
-
-mu_v = 0.001
-sigma_v = jnp.sqrt(0.0005)
-
 x_initial_measurement = sensor(x_true, 0, mu_u, sigma_u, mu_v, sigma_v) # mult_noise
-# x_initial_measurement = sensor(x_true, t=0, cov=sigma_v) # unbiased_fixed_noise
+# x_initial_measurement = sensor(x_true, t=0, std=sigma_v) # unbiased_fixed_noise
 estimator = GEKF(dynamics, dt, mu_u, sigma_u, mu_v, sigma_v, x_init=x_initial_measurement)
-# estimator = EKF(dynamics, dt, x_init=x_initial_measurement, R=sigma_v*jnp.eye(dynamics.state_dim))
+# estimator = EKF(dynamics, dt, x_init=x_initial_measurement, R=jnp.square(sigma_v)*jnp.eye(dynamics.state_dim))
 
 # Define belief CBF parameters
 n = dynamics.state_dim
@@ -60,10 +50,12 @@ delta = 0.001  # Probability of failure threshold
 cbf = BeliefCBF(alpha, beta, delta, n)
 
 # Control params
-clf_gain = 50.0 # CLF linear gain
-clf_slack_penalty = 0.0005
-# clf_slack_penalty = 5
-cbf_gain = 250.0  # CBF linear gain
+u_max = 5.0
+clf_gain = 20.0 # CLF linear gain
+clf_slack_penalty = 50.0
+cbf_gain = 2.5  # CBF linear gain
+
+CBF_ON = True
 
 # Autodiff: Compute Gradients for CLF
 grad_V = grad(clf, argnums=0)  # ∇V(x)
@@ -112,24 +104,30 @@ def solve_qp(b):
 
     A = jnp.array([
         [L_g_V.flatten()[0].astype(float), -1.0], #  LgV u - delta <= -LfV - gamma(V) 
-        # [-Lg_Lf_h.flatten()[0].astype(float), 0.0], # -LgLfh u       <= -[alpha1 alpha2].T @ [Lfh h] + Lf^2h
+        [-Lg_Lf_h.flatten()[0].astype(float), 0.0], # -LgLfh u       <= -[alpha1 alpha2].T @ [Lfh h] + Lf^2h
         [1, 0],
         [0, 1]
     ])
 
     u = jnp.hstack([
         (-L_f_V - clf_gain * V).squeeze(),          # CLF constraint
-        # (rhs).squeeze(),                            # CBF constraint: rhs = -[alpha1 alpha2].T [Lfh h] + Lf^2h
+        (rhs).squeeze(),                            # CBF constraint: rhs = -[alpha1 alpha2].T [Lfh h] + Lf^2h
         u_max, 
         jnp.inf # no upper limit on slack
     ])
 
     l = jnp.hstack([
         -jnp.inf, # No lower limit on CLF condition
-        # -jnp.inf, # No lower limit on CBF condition
+        -jnp.inf, # No lower limit on CBF condition
         -u_max,
         0.0 # slack can't be negative
     ])
+
+    # Remove CBF conditions if CBF is not ON
+    if not CBF_ON:
+        A = jnp.delete(A, 1, axis=0)  # Remove 2nd row
+        u = jnp.delete(u, 1)          # Remove corresponding element in u
+        l = jnp.delete(l, 1)          # Remove corresponding element in l
 
     # Solve the QP using jaxopt OSQP
     sol = solver.run(params_obj=(Q, c), params_eq=A, params_ineq=(l, u)).params
@@ -185,7 +183,7 @@ for t in tqdm(range(T), desc="Simulation Progress"):
     estimator.predict(u_opt)
 
     # update measurement and estimator belief
-    if t > 0 and t%(1/sensor_update_frequency) == 0:
+    if t > 0 and t%(sensor_update_frequency) == 0:
         # obtain current measurement
         x_measured =  sensor(x_true, t, mu_u, sigma_u, mu_v, sigma_v)
         # x_measured = sensor(x_true) # for identity sensor
@@ -266,23 +264,41 @@ plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.legend(fontsize=14)
 plt.title("State Components Over Time: Position and Velocity", fontsize=18)
-plt.grid()
 plt.show()
 
-# # Plot controls
+# Plot controls
 plt.figure(figsize=(10, 10))
+plt.plot(time, np.array([u[0] for u in u_traj]), color='blue', label="u_x")
 plt.plot(time, np.array(cbf_values), color='red', label="CBF")
 plt.plot(time, np.array(clf_values), color='green', label="CLF")
-plt.plot(time, np.array([u[0] for u in u_traj]), color='blue', label="u_x")
 plt.xlabel("Time step (s)")
 plt.ylabel("Value")
 plt.title(f"CBF, CLF, and Control Values ({estimator.name})")
-# Tick labels font size
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
-# Legend font size
 plt.legend(fontsize=14)
 plt.show()
+
+# plt.figure(figsize=(10, 10))
+# GAMMA = 1 # 5 
+# LAMBDA = 1.0 # 1
+# MU = 0.5 # 1 
+# x = x_est[:, 0]         # x over time
+# x_dot = x_est[:, 1]     # ẋ over time
+# diff = x - goal         # x - goal over time
+# sum = (GAMMA*diff + LAMBDA*x_dot)**2
+# plt.plot(time, np.array(clf_values), color='green', label="CLF")
+# plt.plot(time, GAMMA * diff, label="GAMMA * diff", color='blue')
+# plt.plot(time, LAMBDA * x_dot, label="LAMBDA * x_dot", color='red')
+# plt.plot(time, MU * x_dot**2, label="MU * x_dot^2", color='orange')
+# plt.plot(time, sum, label="(GAMMA*diff + LAMBDA*x_dot)**2", color='black')
+# plt.xlabel("Time step (s)")
+# plt.ylabel("Value")
+# plt.title(f"CLF Values ({estimator.name})")
+# plt.xticks(fontsize=14)
+# plt.yticks(fontsize=14)
+# plt.legend(fontsize=14)
+# plt.show()
 
 # Plot CLF (Debug)
 # plt.figure(figsize=(6, 4))
@@ -376,20 +392,25 @@ print(estimator.name)
 print(f"Time Step (dt): {dt}")
 print(f"Number of Steps (T): {T}")
 print(f"Control Input Max (u_max): {u_max}")
-print(f"Sensor Update Frequency (Hz): {sensor_update_frequency}")
+print(f"Sensor Update Frequency (number of iterations): {sensor_update_frequency}")
 
 print("\n--- Environment Setup ---")
 print(f"Obstacle Position (wall_x): {wall_x}")
 print(f"Goal Position (goal_x): {goal_x}")
 print(f"Initial Position (x_init): {x_init}")
 
-print("\n--- Belief CBF Parameters ---")
-print(f"Failure Probability Threshold (delta): {delta}")
-
 print("\n--- Control Parameters ---")
+print(f"u_max: {u_max}")
 print(f"CLF Linear Gain (clf_gain): {clf_gain}")
 print(f"CLF Slack (clf_slack): {clf_slack_penalty}")
 print(f"CBF Linear Gain (cbf_gain): {cbf_gain}")
+if CBF_ON:
+    print("CBF: ON")
+else:
+    print("CBF: OFF")
+
+print("\n--- Belief CBF Parameters ---")
+print(f"Failure Probability Threshold (delta): {delta}")
 
 # Print Metrics
 
@@ -399,9 +420,11 @@ print("Number of estimate exceedances: ", np.sum(x_est > wall_x))
 print("Number of true exceedences", np.sum(x_traj > wall_x))
 print("Max estimate value: ", np.max(x_est))
 print("Max true value: ", np.max(x_traj))
-print("Mean true distance from obstacle: ", np.mean(wall_x - x_est))
+print("Mean est distance from obstacle: ", np.mean(wall_x - x_est))
+print("Mean true distance from obstacle: ", np.mean(wall_x - x_traj))
+print("Mean est distance from goal: ", np.mean(goal_x - x_est))
+print("Mean true distance from goal: ", np.mean(goal_x - x_traj))
 print("Average controller effort: ", np.linalg.norm(u_traj, ord=2))
-print("Cummulative distance to goal: ", np.sum(np.abs(x_traj - wall_x)))
 print(f"{estimator.name} Tracking RMSE: ", np.sqrt(np.mean((x_traj - x_est) ** 2)))
 
 